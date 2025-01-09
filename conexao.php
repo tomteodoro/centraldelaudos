71fa68667bd0c4d703e6ee101b86bac1b58ca940
tomteodoro <wellintoncteodoro@gmail.com>
2025-01-09 14:13:04 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<!--Criado por Wellinton C. Teodoro em MAR/2023 - wteodoro@amesaojoao.unicamp.br-->
-
-<?php
-error_reporting(0);
-?>
-
-<?php
-$conexao = pg_connect("host=143.106.234.11 port=7002 dbname=salutem user=ame password=ame123") or die("Não foi possível realizar conexão com o banco de dados!<br>Tente novamente.\n <br><br><a href='./'>Voltar</a>");
-?>
